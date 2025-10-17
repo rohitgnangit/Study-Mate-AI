8a256457e2b034ee1e27cbaf1fffd92b8c29ef4a
rohitgnangit <rohitmuppana55@gmail.com>
2025-10-17 23:12:29 +0530

    Home Page UI

---
+++ components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="bg-black border-b border-slate-700 h-12 flex items-center">
+        <div className="logo font-semi-bold text-white px-3 text-lg">StudyMate-AI</div>
+    </nav>
+  )
+}
+
+export default Navbar
