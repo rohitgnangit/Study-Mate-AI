323e933b9f7a3037f1bb981db5a1294057896cae
rohitgnangit <rohitmuppana55@gmail.com>
2025-10-19 22:43:10 +0530

    Integrated with spline 3D Animation

--- app/page.js
+++ app/page.js
@@ -1,11 +1,17 @@
 "use client"
 import Spline from "@splinetool/react-spline";
+import Link from "next/link";
 
 export default function LandingPage() {
   return (
     <>
-      <div className="landingPage min-h-screen text-white relative h-full w-full bg-slate-950"><div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
-        <div className="mx-auto flex flex-col w-[85%] justify-center items-center px-8 py-10">
+      <div className="landingPage flex flex-col justify-center items-center min-h-screen text-white h-full w-full bg-slate-950 relative"><div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
+        <div className="animation w-full">
+          <Spline
+            scene="https://prod.spline.design/WxOjLlxJjVnrRXpE/scene.splinecode"
+          />
+        </div>
+        <div className="mx-auto flex flex-col w-[85%] justify-center items-center px-8 py-10 absolute">
           <div className="heading py-5 w-[80%]">
             <h1 className="text-5xl font-semibold text-gray-200 pt-20 text-center">Your Smart Study Companion</h1>
             <h1 className="text-5xl font-semibold text-gray-200 pt-2 pb-8 text-center">with AI</h1>
@@ -13,11 +19,13 @@ export default function LandingPage() {
             <p className="text-xl font text-gray-400 text-center">instant explanations. Study smarter, not harder, with AI that</p>
             <p className="text-xl font text-gray-400 text-center">understands your learning material.</p>
           </div>
-          <div className="animation w-[85%]">
-            <Spline className=""
-              scene="https://prod.spline.design/nyiN0IGMkdPj0EA5/scene.splinecode"
-            />
-          </div>
+          <Link href="/signup">
+          <button type="button" className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-1 focus:ring-gray-100 font-medium rounded-lg text-sm px-8 py-2.5 dark:bg-gray-800 dark:text-gray-200 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 mt-5 cursor-pointer">Get Started</button>
+          </Link>
+        </div>
+
+        <div className="bottom">
+          How it works ?
         </div>
 
       </div>
--- components/Navbar.js
+++ components/Navbar.js
@@ -8,6 +8,15 @@ const Navbar = () => {
       <Link href="/">
         <div className="logo font-semi-bold text-white px-3 text-lg"><Image src="/LernovA.png" alt="logo" width={130} height={130} /></div>
       </Link>
+      <div className="btns">
+        <Link href="/signup">
+        <button className="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-gray-200 focus:ring-1 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
+          <span className="relative px-4.5 py-1.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent cursor-pointer">
+            Sign in
+          </span>
+        </button>
+        </Link>
+      </div>
     </nav>
   )
 }
