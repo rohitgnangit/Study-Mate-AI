5da5b5b3c28a39300f4ccdfbbcb721759dd4b338
rohitgnangit <rohitmuppana55@gmail.com>
2025-10-21 22:38:11 +0530

    Log out popup created

--- app/home/page.js
+++ app/home/page.js
@@ -1,20 +1,28 @@
 'use client'
+
+import LogoutPopUp from "@/components/LogoutPopUp";
+import Image from "next/image";
 import { useSession, signIn, signOut } from "next-auth/react";
 import { redirect } from 'next/navigation'
-import Image from "next/image";
+import { useState } from "react";
 
 
 export default function Home() {
   const { data: session } = useSession();
+  const [isLogout, setIsLogout] = useState(false)
+
   const email = session?.user?.email || "User";
   const user = email.split("@")[0].match(/^[a-zA-Z]+/)[0];
   const name = user.charAt(0).toUpperCase() + user.slice(1);
-  console.log("NAME",name);
+
   if (!session) {
     redirect("/")
   }
+
   return (
     <>
+    {isLogout && <LogoutPopUp isLogout = {isLogout} setIsLogout = {setIsLogout}/>}
+
       <div className="home bg-gray-950 min-h-screen flex">
         {/* Left Container */}
         <div className="left flex flex-col justify-between text-white bg-gray-900 w-1/4 float-left p-5">
@@ -29,8 +37,8 @@ export default function Home() {
             </div>
           </div>
           <div className="logout">
-            <button onClick={() => { signOut() }} className="relative inline-flex items-center justify-center p-[1.4px] overflow-hidden text-xs text-gray-400 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-1 dark:focus:ring-cyan-800 cursor-pointer my-5">
-              <span className="relative px-2.5 py-1 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
+            <button onClick={() => { setIsLogout(true) }} className="relative inline-flex items-center justify-center p-[1.4px] overflow-hidden text-xs text-gray-400 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-1 dark:focus:ring-cyan-800 cursor-pointer my-5">
+              <span className="relative px-3 py-1.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
                 Logout
               </span>
             </button>
@@ -45,11 +53,16 @@ export default function Home() {
                 Hello {name}
               </h1>
             </div>
-            <div className="inputField h-15 w-full my-10 rounded-2xl border border-slate-500 shadow-sm shadow-blue-200">
-              <input type="text" className="h-full w-full rounded-2xl px-4 text-white" placeholder="ask something ?" />
+            <div className="inputField flex justify-between items-center h-15 w-full my-10 rounded-2xl border border-slate-500 shadow-sm shadow-blue-200">
+              {/* Chat Input */}
+              <input type="text" className="outline-none h-full w-[95%] rounded-2xl px-4 text-white" placeholder="ask something ?" />
+              {/* Send Button */}
+              <div className="send h-10 w-10 flex justify-center items-center rounded-full mr-3">
+                <button className="w-full h-full flex justify-center items-center rounded-full cursor-pointer bg-gray-800 hover:bg-gray-700"><Image src="/send.png" alt="send icon" height={19} width={19}></Image></button>
+              </div>
             </div>
             <div className="fileUpload">
-              <button type="button" className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-1 focus:ring-gray-100 font-medium rounded-lg text-sm px-8 py-2.5 dark:bg-gray-800 dark:text-gray-200 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 cursor-pointer"><span className="flex justify-between items-center gap-4"><Image src="/FileUpload.png" alt="Upload File" width={20} height={20}></Image>Upload File</span></button>
+              <button type="button" className="text-gray-900 bg-white border border-gray-900 focus:outline-none hover:bg-gray-100 focus:ring-1 focus:ring-gray-100 font-medium rounded-lg text-sm px-7 py-2.5 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 dark:hover:bg-gray-800 dark:hover:border-gray-600 dark:focus:ring-gray-800 cursor-pointer"><span className="flex justify-between items-center gap-4"><Image src="/FileUpload.png" alt="Upload File" width={20} height={20}></Image>Upload File</span></button>
             </div>
           </div>
         </div>
---
+++ components/LogoutPopUp.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useSession, signOut } from "next-auth/react";
+
+
+const LogoutPopUp = ( { isLogout, setIsLogout } ) => {
+    const { data: session } = useSession();
+    const email = session?.user?.email || "User";
+
+    const handleCancel = () => {
+      setIsLogout(false);
+    }
+
+    const handleLogout = () => {
+      signOut();
+      setIsLogout(false);
+    }
+    
+    
+
+    return (
+        <div className=" h-screen w-screen bg-transparent backdrop-blur-sm fixed left-0 top-0 z-50  flex justify-center items-center">
+            <div className="min-h-[25%] min-w-[30%] flex flex-col justify-center gap-7 items-center rounded-lg text-gray-200 bg-gray-800">
+                <div className="head w-full flex flex-col items-center gap-2">
+                    <h1 className="text-lg font-bold">Are you really want to Log out ?</h1>
+                    <p className="">Log out of Learnova as {email} ?</p>
+                </div>
+                <div className="btns w-full flex justify-end gap-5 px-5">
+                    <button onClick={ ()=>{ handleCancel() }} className="px-3 py-2 text-sm font-medium bg-gray-600 rounded-lg hover:bg-gray-700 cursor-pointer">Cancel</button>
+                    <button onClick={ ()=>{ handleLogout() }} className="px-3 py-2 text-sm font-medium bg-gray-900 rounded-lg hover:bg-gray-700 cursor-pointer">Log out</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LogoutPopUp
